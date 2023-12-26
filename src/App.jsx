e620d7ef4bf6ee5d0620212c886268c4eff4af70
alla0810 <kyosook.shin@gmail.com>
2023-12-25 20:32:55 -0600

    editing contact page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 import Footer from './components/Footer'
+import {useEffect} from 'react';
 
 function App() {
+  const defaultTitle = 'KyoSook Shin';
+
+  useEffect( () => {
+    document.title = defaultTitle;
+  }, [defaultTitle])
+
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
-      <header></header>
+      <header>
+        <title>KyoSook Shin</title>
+      </header>
       <Nav />
       <main className="mx-3">
         <Outlet />
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,25 +1,57 @@
+import {useState} from 'react';
+import '../assets/Contact.css'
+
 
 export default function Contact() {
 
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');  
 
+  const handleInputChange = (e) => {
+    const {target} = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email')
+    {
+      setEmail(inputValue);
+    }
+    else if (inputType === 'name')
+    {
+      setName(inputValue);      
+    }
+    else if (inputType === 'message')
+    {
+      setMessage(inputValue);
+    }
+
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+  }
 
 
   return (
     <div>
       <h1>Contact</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <form onSubmit={handleFormSubmit}>
+        Name: 
+        <br></br>
+        <input className="contact_input" value={name} name="name" onChange={handleInputChange} type="text" placeholder=""/>        
+
+        Email address:
+        <br></br>
+        <input className="contact_input" value={email} name="email" onChange={handleInputChange} type="email" placeholder=""/>
+        Message:
+        <br></br>        
+        <input className="contact_message" value={message} name="message" onChange={handleInputChange} type="text" placeholder=""/>
+        <button type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }

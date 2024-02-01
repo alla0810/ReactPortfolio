aa7fe34632853c0378ce2c274b371e2d515d82a0
alla0810 <kyosook.shin@gmail.com>
2024-02-01 09:12:32 -0600

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
-import Footer from './components/Footer'
 import {useEffect} from 'react';
 
 function App() {
